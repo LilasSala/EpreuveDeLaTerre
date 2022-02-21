25a0eae095148f283dcc49e39a410489f013ef54
LilasSala <lilassala@icloud.com>
2022-02-21 14:54:17 +0100

    init

---
+++ terre01.js
@@ -0,0 +1,6 @@
+const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let result = '';
+for (let i = 0; i < alphabet.length; i++) {
+  result += alphabet[i];
+}
+console.log(result + "\n");
